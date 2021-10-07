0b290ce5d0af448872035bb1def7123e0a25ab57
cycsd <cycsd9211@gmail.com>
2021-10-08 01:18:54 +0800

    1. Add View Component 2. 將Layout依功能分至ViewComponet的各個頁面

---
+++ ViewComponents/IndexComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VaccinationReservationPlatForm.ViewComponents
+{
+    [ViewComponent(Name = "Index")]
+    public class IndexComponent:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string controller)
+        {
+            return View(controller);
+        }
+    }
+}
+
