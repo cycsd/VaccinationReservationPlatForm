7476a764882ae8b7143545311b6ad77fd51ae281
DESKTOP-9C36BGM\User <jerry86915@gmail.com>
2021-10-19 11:13:12 +0800

    新增ViewModels和Login.cshtml

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using VaccinationReservationPlatForm.Models;
+using VaccinationReservationPlatForm.ViewModels;
 
 namespace VaccinationReservationPlatForm.Controllers
 {
@@ -19,6 +22,30 @@ namespace VaccinationReservationPlatForm.Controllers
         {
             _logger = logger;
             _context = vaccinationBookingSystemContext;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(CLoginViewModel model)
+        {
+            Person cust = (new VaccinationBookingSystemContext()).People.FirstOrDefault(
+                c => c.PersonIdentityId.Trim().Equals(model.txtPersonIdentityID) && c.PersonHealthId.Trim().Equals(model.txtPersonHealthID));
+            if (cust != null && cust.PersonIdentityId.Trim().Equals(model.txtPersonIdentityID) && cust.PersonHealthId.Trim().Equals(model.txtPersonHealthID))
+            {
+                string json = JsonSerializer.Serialize(cust);
+                HttpContext.Session.SetString(CDictionary.SK_LOGIN_CLIENT, json);
+                ViewBag.Error = "";
+                return RedirectToAction("Index");
+            }
+            if (cust == null)
+            {
+                ViewBag.Error = "身分證或健保卡卡號錯誤";
+            }
+            return View(cust);
         }
 
         public IActionResult Index()
---
+++ ViewModels/CUserInfoModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VaccinationReservationPlatForm.ViewModels
+{
+    public class CUserInfoModel
+    {
+        [DisplayName("身分證字號")]
+        public string labPersonIdentityID { get; set; }
+        [DisplayName("健保卡卡號")]
+        public string labPersonHealthID { get; set; }
+        [DisplayName("郵遞區號")]
+        public string labCountyPostalCode { get; set; }
+        [DisplayName("居住地址")]
+        public string txtPersonAdress { get; set; }
+        [DisplayName("姓名")]
+        public string labPersonName { get; set; }
+        [DisplayName("出生日期")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0: yyyy-MM-dd}")]
+        public DateTime labPersonBirthday { get; set; }
+        [DisplayName("手機號碼")]
+        public string txtPersonCellphoneNumber { get; set; }
+        [DisplayName("電子信箱")]
+        public string txtPersonMail { get; set; }
+        [DisplayName("職業")]
+        public string txtPersonJob { get; set; }
+        [DisplayName("性別")]
+        public string labPersonSex { get; set; }
+
+
+    }
+}
