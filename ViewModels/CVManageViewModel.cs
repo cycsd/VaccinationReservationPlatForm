cc658e0126efaf320e11d6b82954b379e26ae689
Penny0916 <p1995916@gmail.com>
2021-11-02 09:01:20 +0800

    佩蓉 新增頁面

---
+++ ViewModels/CVManageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VaccinationReservationPlatForm.ViewModels
+{
+    public class CVManageViewModel
+    {
+        public int? VaccineExp { get; set; }
+        public string DiseaseName { get; set; }
+        public int VaccinePsiid { get; set; }
+        public int? HospitalId { get; set; }
+        public int? VaccineId { get; set; }
+        public string VaccineName { get; set; }
+        public int? VaccinePsiquantity { get; set; }
+        public string VaccinePsistateRemark { get; set; }
+        public string BatchNumber { get; set; }
+        public DateTime? VaccinePsirecordTime { get; set; }
+        public DateTime? VaccinePsimfDate { get; set; }
+        public DateTime? VaccinePsiexpDate { get; set; }
+      
+    }
+}
+
---
+++ ViewModels/Work2ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VaccinationReservationPlatForm.Models
+{
+    public class Work2ViewModel
+    {
+        public int PersonId { get; set; }
+        public string PersonName { get; set; }
+        public string PersonIdentityId { get; set; }       
+        public int? Vbnumber { get; set; }       
+        public string VbcheckRemark { get; set; }
+        public int? VaccineSerialNumber { get; set; }        
+        public DateTime? VbbookingDate { get; set; }       
+        public DateTime? VbappointmentDate { get; set; }
+        public TimeSpan? VbbookingTime { get; set; }
+    }
+}
